9bcb1bf4d7eec593de01f0edbc54112b25448866
Renganathanc <nathancranga4@gmail.com>
2024-08-27 17:40:53 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,151 @@
+
+import './App.css'
+import bishop from './assets/icons8-bishop-100.png'
+import king from './assets/icons8-king-96.png'
+import knight from './assets/icons8-knight-96.png'
+import pawn from './assets/icons8-pawn-90.png'
+import queen from './assets/icons8-queen-100.png'
+import rook from './assets/icons8-rook-96.png'
+
+function App() {
+  
+  return (
+    <>
+    <div>
+      <div className="container-fluid"style={{'margin-left':'250px'}}  >
+        <div className="class ">
+            <div className="white">
+              <img src={rook} alt="" />
+            </div>
+            <div className="black">
+              <img src={knight} alt="" />
+            </div>
+            <div className="white">
+              <img src={bishop} alt="" />
+            </div>
+            <div className="black">
+              <img src={queen} alt="" />
+            </div>
+            <div className="white">
+              <img src={king} alt="" />
+            </div>
+            <div className="black">
+              <img src={bishop} alt="" />
+            </div>
+            <div className="white">
+              <img src={knight} alt="" />
+            </div>
+            <div className="black">
+              <img src={rook} alt="" />
+            </div>
+            <div className="black">
+              <img src={pawn} alt="" />
+            </div>
+            <div className="white">
+              <img src={pawn} alt="" />
+            </div>
+            <div className="black">
+              <img src={pawn} alt="" />
+            </div>
+            <div className="white">
+              <img src={pawn} alt="" />
+            </div>
+            <div className="black">
+              <img src={pawn} alt="" />
+            </div>
+            <div className="white">
+              <img src={pawn} alt="" />
+            </div>
+            <div className="black">
+              <img src={pawn} alt="" />
+            </div>
+            <div className="white">
+              <img src={pawn} alt="" />
+            </div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white">
+            </div>
+            <div className="white">
+              <img src={pawn} alt="" />
+            </div>
+            <div className="black">
+              <img src={pawn} alt="" />        
+            </div>
+            <div className="white">
+              <img src={pawn} alt="" />
+            </div>
+            <div className="black">
+              <img src={pawn} alt="" />
+            </div>
+            <div className="white">
+              <img src={pawn} alt="" />
+            </div>
+            <div className="black"></div>
+              <div className="white">
+            <img src={pawn} alt="" />
+            </div>
+            <div className="black">
+              <img src={pawn} alt="" />
+            </div>
+            <div className="black">
+              <img src={rook} alt="" />
+            </div>
+            <div className="white">
+              <img src={knight} alt="" />
+            </div>
+            <div className="black">
+              <img src={bishop} alt="" />
+            </div>
+            <div className="white">
+              <img src={queen} alt="" />
+            </div>
+            <div className="black">
+              <img src={king} alt="" />
+            </div>
+            <div className="white">
+              <img src={bishop} alt="" />
+            </div>
+            <div className="black">
+              <img src={knight} alt="" />
+            </div>
+            <div className="white">
+              <img src={rook} alt="" />
+            </div>
+        </div>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default App
